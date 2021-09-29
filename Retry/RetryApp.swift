97500d53943512dbbf5846b06a74b114a005b871
Rexford Machu <rexfordmachu@gmail.com>
2021-09-29 23:25:53 +0000

    Initial Commit

---
+++ Retry/RetryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetryApp.swift
+//  Retry
+//
+//  Created by Rexford Machu on 9/29/21.
+//
+
+import SwiftUI
+
+@main
+struct RetryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
